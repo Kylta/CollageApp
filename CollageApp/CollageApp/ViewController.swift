165d488af7cdaa7890d328cb0a90fb281649bb79
Kylta <Christophe.bugnon02@gmail.com>
2018-01-31 11:39:58 +0100

    Explain method performDrop before makes closure

--- CollageApp/CollageApp/ViewController.swift
+++ CollageApp/CollageApp/ViewController.swift
@@ -18,6 +18,14 @@ class ViewController: UIViewController, UIDropInteractionDelegate {
         view.addInteraction(UIDropInteraction(delegate: self))
     }
     
+    // Method for had an array of item drop in the view
+    func dropInteraction(_ interaction: UIDropInteraction, performDrop session: UIDropSession) {
+        // Loop for session.items because it's an array
+        for dragItem in session.items {
+            
+        }
+    }
+    
     // Method for copy Drip in the session (.move is take an object from an other app and put it the drop)
     func dropInteraction(_ interaction: UIDropInteraction, sessionDidUpdate session: UIDropSession) -> UIDropProposal {
         return UIDropProposal(operation: .copy)
